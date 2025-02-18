7615924d9085801beb8c20ba9321ef8aab5f24c8
hardikkandoriya <kandoriyahardik2397@gmail.com>
2025-02-18 12:09:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (61.87%) - LeetSync

---
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -0,0 +1,11 @@
+class Solution:
+  def invertTree(self, root):
+    if not root:
+      return None
+
+    left = root.left  
+    right = root.right
+    root.left = self.invertTree(right)  #assign tree's actual right node as left node .
+    root.right = self.invertTree(left) #assign trees actual left node as right node .
+    return root
+
